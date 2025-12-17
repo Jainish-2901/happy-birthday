379989f9f90a09ddd990c6be8ad0ba5d54935f67
Jainish Dabgar <jainishdabgar2901@gmail.com>
2025-12-17 19:10:34 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -275,7 +275,7 @@ function App() {
                 {/* --- FIX 2: Use data from .env file --- */}
                 {/* The text you want (like the Hindi text) MUST be in your .env file */}
                 <span id="wishText">
-                  Dear <span id="name" className="name" ref={nameRef}>{data.name}</span>
+                  Dear <span id="name" className="name" ref={nameRef}>{data.name}</span>,<br />
                   कोई दिन खास नहीं चाहिए<br />
                   याद दिलाने के लिए कि तू कितनी special है ✨<br />
                   जहाँ जाती है, वहाँ मुस्कान छोड़ देती है 🌸<br />
